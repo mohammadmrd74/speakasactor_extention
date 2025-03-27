58d2004335f575a9fdbbafecdba5b8355955db02
Mohammad Hasan Moradi <mohammadmrd74@gmail.com>
2025-03-27 23:51:47 +0330

    better styling

--- content.js
+++ content.js
@@ -11,6 +11,7 @@ let isListening = false;
 let currentLang = "en-US";
 let isActive = true;
 let incorrectCount = 0;
+let button = '';
 
 function createModal() {
   let modal = document.createElement("div");
@@ -37,12 +38,68 @@ function showModal() {
   if (modal) {
     modal.style.display = "block";
     modal.innerHTML = `
-      <p>Speak Now!</p>
-      <p>${lastSubtitle}</p>
-      <p style="margin-top:10px">what you said: ${userText}</p>
-      <div>score: ${userScore}</div>
+      <div style="
+        font-size: 22px;
+        text-align: center;
+        font-weight: 700;
+      ">Speak Now!</div>
+      <div style="
+        margin-top: 7px;
+        border: 1px solid;
+        padding: 11px;
+        border-radius: 8px;
+        ">${lastSubtitle}</div>
+      <div style="margin-top:10px;margin-top: 11px;border: 1px dashed;padding: 11px;color: #dbdfe7;border-radius: 8px;">You said: ${userText}</div>
+      <div 
+        style="text-align: center;
+        margin-top: 12px;
+        font-size: 30px;"
+        >score: ${userScore}</div>
       <div style="margin-top:10px;color:red;">${warningText}</div>
   `;
+  }
+}
+
+function showButton() {
+  let modal = document.getElementById("speakasactor_modal");
+  if (modal) {
+    button = document.createElement("button");
+    button.textContent = "Continue";
+    button.style.marginTop = "10px";
+    button.style.padding = "8px 16px";
+    button.style.border = "none";
+    button.style.borderRadius = "5px";
+    button.style.background = "#ffffff";
+    button.style.color = "#000";
+    button.style.cursor = "pointer";
+
+    function startVideo() {
+      video.play();
+      subtitleInterval = setInterval(getSubtitles, 5000);
+      modal.style.display = "none";
+      button.remove();
+    }
+
+    button.onclick = startVideo;
+
+    translateButton = document.createElement("a");
+    translateButton.innerHTML = "Translate";
+    translateButton.style.marginTop = "10px";
+    translateButton.style.marginLeft = "5px";
+    translateButton.style.marginRight = "5px";
+    translateButton.style.padding = "8px 16px";
+    translateButton.style.border = "none";
+    translateButton.style.borderRadius = "5px";
+    translateButton.style.background = "#ffffff";
+    translateButton.style.textDecoration = "none";
+    translateButton.style.fontSize = "13px";
+    translateButton.style.color = "#000";
+    translateButton.style.cursor = "pointer";
+    translateButton.setAttribute('href', `https://translate.google.com/?sl=auto&tl=en&text=${lastSubtitle}&op=translate`);
+    translateButton.setAttribute('target', `_blank`);
+
+    modal.appendChild(button);
+    modal.appendChild(translateButton);
   }
 }
 
@@ -66,24 +123,21 @@ async function getSubtitles() {
   };
   const subtitleElements = document.querySelectorAll(".ytp-caption-segment");
   let text = Array.from(subtitleElements).map(el => el.innerText.trim()).join(" ");
-  if (text) {
+  if (text && text.length > 9) {
     clearInterval(subtitleInterval);
     lastSubtitle = "";
     while (true) {
       const subtitleElements = document.querySelectorAll(".ytp-caption-segment");
       text = Array.from(subtitleElements).map(el => el.innerText.trim()).join(" ");
       let similarityScore = findSimilarity(text, lastSubtitle);
-      console.log('text', text);
-      console.log("lastSubtitle", lastSubtitle);
       if (lastSubtitle && similarityScore < 0.65) {
-        console.log("lastSubtitle", lastSubtitle);
         video.pause();
         showModal();
         startSpeechRecognition(lastSubtitle);
         break;
       }
 
-      lastSubtitle = text;
+      lastSubtitle = text.replace(/–/g, "");
       await waitFor(500);
     }
   }
@@ -137,7 +191,7 @@ function startSpeechRecognition(expectedText, repeat = 0) {
       });
     });
     showModal();
-    if (similarity >= 0.8) {
+    if (similarity >= 0.7) {
       console.log("✅ Correct! Resuming...");
       incorrectCount = 0;
       warningText = "";
@@ -166,10 +220,11 @@ function startSpeechRecognition(expectedText, repeat = 0) {
     if (isActive && incorrectCount !== 1) {
       setTimeout(() => {
         isListening = false;
-        closeModal();
-        video.play();
-        subtitleInterval = setInterval(getSubtitles, 5000);
-      }, 2000);
+        // closeModal();
+        // video.play();
+        // subtitleInterval = setInterval(getSubtitles, 5000);
+        showButton();
+      }, 1000);
     } else {
       console.log("REPEAT");
 
